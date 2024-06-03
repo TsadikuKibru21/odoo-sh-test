cd85492471be9dfe54261fe008cbe3c79d425514
Melkam Zeyede <zeyede8@gmail.com>
2024-06-03 16:16:39 +0300

    removed comment from _finalizeValidation

--- custom_addons/pos_etta/static/src/app/payment_screen/payment_screen.js
+++ custom_addons/pos_etta/static/src/app/payment_screen/payment_screen.js
@@ -17,79 +17,79 @@ patch(PaymentScreen.prototype, {
         }
 
         return false;
-    },   
-    // async _finalizeValidation() {
-    //     if (this.currentOrder.is_paid_with_cash() || this.currentOrder.get_change()) {
-    //         if (window.Android != undefined) {
-    //             if (window.Android.isAndroidPOS()) {
-    //                 window.Android.openCashDrawer();
-    //             }
-    //         }
-    //         this.hardwareProxy.openCashbox();
-    //     }
+    },
+    async _finalizeValidation() {
+        if (this.currentOrder.is_paid_with_cash() || this.currentOrder.get_change()) {
+            if (window.Android != undefined) {
+                if (window.Android.isAndroidPOS()) {
+                    window.Android.openCashDrawer();
+                }
+            }
+            this.hardwareProxy.openCashbox();
+        }
 
-    //     this.currentOrder.date_order = luxon.DateTime.now();
-    //     for (const line of this.paymentLines) {
-    //         if (!line.amount === 0) {
-    //             this.currentOrder.remove_paymentline(line);
-    //         }
-    //     }
+        this.currentOrder.date_order = luxon.DateTime.now();
+        for (const line of this.paymentLines) {
+            if (!line.amount === 0) {
+                this.currentOrder.remove_paymentline(line);
+            }
+        }
 
-    //     //TODO : printing of fiscal receipt done here
-    //     var fiscalPrintResult = await this.currentOrder.printFiscalReceipt();
+        //TODO : printing of fiscal receipt done here
+        var fiscalPrintResult = await this.currentOrder.printFiscalReceipt();
 
-    //     if (fiscalPrintResult || this.isFiscalPrinted()) {
-    //         this.currentOrder.finalized = true;
+        if (fiscalPrintResult || this.isFiscalPrinted()) {
+            this.currentOrder.finalized = true;
 
-    //         // 1. Save order to server.
-    //         this.env.services.ui.block();
-    //         const syncOrderResult = await this.pos.push_single_order(this.currentOrder);
-    //         this.env.services.ui.unblock();
+            // 1. Save order to server.
+            this.env.services.ui.block();
+            const syncOrderResult = await this.pos.push_single_order(this.currentOrder);
+            this.env.services.ui.unblock();
 
-    //         if (syncOrderResult instanceof ConnectionLostError) {
-    //             this.pos.showScreen(this.nextScreen);
-    //             return;
-    //         } else if (!syncOrderResult) {
-    //             return;
-    //         }
+            if (syncOrderResult instanceof ConnectionLostError) {
+                this.pos.showScreen(this.nextScreen);
+                return;
+            } else if (!syncOrderResult) {
+                return;
+            }
 
-    //         try {
-    //             // 2. Invoice.
-    //             if (this.shouldDownloadInvoice() && this.currentOrder.is_to_invoice()) {
-    //                 if (syncOrderResult[0]?.account_move) {
-    //                     await this.report.doAction("account.account_invoices", [
-    //                         syncOrderResult[0].account_move,
-    //                     ]);
-    //                 } else {
-    //                     throw {
-    //                         code: 401,
-    //                         message: "Backend Invoice",
-    //                         data: { order: this.currentOrder },
-    //                     };
-    //                 }
-    //             }
-    //         } catch (error) {
-    //             if (error instanceof ConnectionLostError) {
-    //                 Promise.reject(error);
-    //                 return error;
-    //             } else {
-    //                 throw error;
-    //             }
-    //         }
+            try {
+                // 2. Invoice.
+                if (this.shouldDownloadInvoice() && this.currentOrder.is_to_invoice()) {
+                    if (syncOrderResult[0]?.account_move) {
+                        await this.report.doAction("account.account_invoices", [
+                            syncOrderResult[0].account_move,
+                        ]);
+                    } else {
+                        throw {
+                            code: 401,
+                            message: "Backend Invoice",
+                            data: { order: this.currentOrder },
+                        };
+                    }
+                }
+            } catch (error) {
+                if (error instanceof ConnectionLostError) {
+                    Promise.reject(error);
+                    return error;
+                } else {
+                    throw error;
+                }
+            }
 
-    //         // 3. Post process.
-    //         if (
-    //             syncOrderResult &&
-    //             syncOrderResult.length > 0 &&
-    //             this.currentOrder.wait_for_push_order()
-    //         ) {
-    //             await this.postPushOrderResolve(syncOrderResult.map((res) => res.id));
-    //         }
+            // 3. Post process.
+            if (
+                syncOrderResult &&
+                syncOrderResult.length > 0 &&
+                this.currentOrder.wait_for_push_order()
+            ) {
+                await this.postPushOrderResolve(syncOrderResult.map((res) => res.id));
+            }
 
-    //         await this.afterOrderValidation(!!syncOrderResult && syncOrderResult.length > 0);
-    //     }
-    //     else {
+            await this.afterOrderValidation(!!syncOrderResult && syncOrderResult.length > 0);
+        }
+        else {
 
-    //     }
-    // }
+        }
+    }
 });
