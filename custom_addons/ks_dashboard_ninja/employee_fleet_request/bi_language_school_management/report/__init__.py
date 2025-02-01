69e5579e8754488ebe6ba2122280458e55b74901
TsadikuKibru21 <tsadikuk@gmail.com>
2025-02-01 11:19:33 +0300

    dffgd

--- custom_addons/ks_dashboard_ninja/employee_fleet_request/bi_language_school_management/report/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import language_class_report
--- custom_addons/ks_dashboard_ninja/employee_fleet_request/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import employee_fleet_request
-from . import fleet_vehicle
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
